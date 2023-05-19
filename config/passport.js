f65b40976e1a136c188af8ec218e9eef8a40984f
Fabio Teixeira <53839583+fabio-teixeira-ifsp@users.noreply.github.com>
2023-05-19 10:07:16 -0300

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var findOrCreate = require('mongoose-findorcreate');
+var mongoose = require('mongoose');
+
+module.exports = function() {
+
+    var Usuario = mongoose.model('Usuario');
+
+    passport.use(new GitHubStrategy({
+        clientID: 'de4c2e6477ed3b643981',
+        clientSecret: '2db5a638489773f791698b8dfb6d14fcecd36ecd',
+        callbackURL: 'https://aula-15-ac-solution.dswa5.repl.co/auth/github/callback'
+    }, function(accessToken, refreshToken, profile, done) {
+
+        Usuario.findOrCreate({ "login": profile.username }, { "nome": profile.username },
+            function(erro, usuario) {
+                if (erro) {
+                    return done(erro);
+                }
+                return done(null, usuario);
+            }
+        );
+    }));
+
+    passport.serializeUser(function(usuario, done) {
+        done(null, usuario._id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        Usuario.findById(id).exec()
+            .then(function(usuario) {
+                done(null, usuario);
+            });
+    });
+};
